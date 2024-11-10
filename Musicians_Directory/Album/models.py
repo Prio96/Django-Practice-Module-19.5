cf0d6c7e17911d8a305c2a73557950c2abe3cc1a
Prio <prio96@icloud.com>
2024-11-11 01:50:51 +0600

    Added Album Model

--- Musicians_Directory/Album/models.py
+++ Musicians_Directory/Album/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from Musician.models import Musician
 # Create your models here.
+class Album(models.Model):
+    album=models.CharField(max_length=255)
+    musician=models.ForeignKey(Musician,on_delete=models.CASCADE,related_name="album")
+    release_date=models.DateField(auto_now_add=True)
+    
