a057832cd92388bb70b1a6f326f0f3eb14b3e465
Prio <prio96@icloud.com>
2024-11-13 02:11:35 +0600

    Set up models and installed necessities

--- Musicians_Directory/Album/models.py
+++ Musicians_Directory/Album/models.py
@@ -1,8 +1,19 @@
 from django.db import models
+from django.core.validators import MinValueValidator,MaxValueValidator
 from Musician.models import Musician
 # Create your models here.
+class Rating(models.IntegerChoices):
+    one=1,'1'
+    two=2,'2'
+    three=3,'3'
+    four=4,'4'
+    five=5,'5'
 class Album(models.Model):
-    album=models.CharField(max_length=255)
-    musician=models.ForeignKey(Musician,on_delete=models.CASCADE,related_name="album")
+    title=models.CharField(max_length=255)
+    musician=models.ForeignKey(Musician,on_delete=models.CASCADE,related_name="albums")  
     release_date=models.DateField(auto_now_add=True)
+    rating=models.PositiveSmallIntegerField(
+        choices=Rating.choices,
+        validators=[MinValueValidator(1),MaxValueValidator(5)]
+    )
     
--- Musicians_Directory/Musician/models.py
+++ Musicians_Directory/Musician/models.py
@@ -2,4 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Musician(models.Model):
-    pass
+    first_name=models.CharField(max_length=40)
+    last_name=models.CharField(max_length=40)
+    email=models.EmailField()
+    phone_number=models.CharField(max_length=20)
+    instrument_type=models.CharField(max_length=50)
+
+    
